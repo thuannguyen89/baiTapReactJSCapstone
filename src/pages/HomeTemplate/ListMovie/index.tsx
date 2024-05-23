2ba3590ef5f1d46ac7d0acd8e5d8e2e35de84c04
minhtruong0803 <vtrg0803@gmail.com>
2024-05-23 22:08:39 +0700

    Update code MinhTruong_branch

---
+++ src/pages/HomeTemplate/ListMovie/index.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { actFetchListData } from "./duck/action";
+import { RootState } from "../../../store";
+import MovieComponent from "./movie";
+
+export default function ListMovie() {
+  const dispatch: any = useDispatch();
+
+  const { loading, data } = useSelector(
+    (state: RootState) => state.listMovieReducer
+  );
+
+  useEffect(() => {
+    dispatch(actFetchListData());
+  }, []);
+
+  const renderListMovie = () => {
+    if (loading) return <div>Loading...</div>;
+
+    if (data && data.length > 0) {
+      return data.map((movie) => <MovieComponent movie={movie} />);
+    }
+  };
+
+  return (
+    <div className="container">
+      <h3>List-Movie Page</h3>
+      <div className="row">{renderListMovie()}</div>
+    </div>
+  );
+}
---
+++ src/store/types.ts
@@ -0,0 +1,17 @@
+export type AppState<T> = {
+    loading: boolean;
+    data: T[] | null;
+    error: any;
+  };
+  
+  export type Action = {
+    type: string;
+    payload?: any;
+  };
+  
+  export type AppStateDetails<K> = {
+    loading: boolean;
+    data: K | null;
+    error: any;
+  };
+  
